5a50a142abff694de38ec21904b97d9c597fdbd0
Ignacio Rivas <rivasign@gmail.com>
2015-03-03 17:05:18 +0000

    Initial commit

--- README.md
+++ README.md
@@ -1,1 +1,20 @@
-# leaflet-marker-cluster
+Leaflet-Marker-Cluster
+=====================
+
+Forked from [.eaflet.markercluster](https://github.com/Leaflet/Leaflet.markercluster).
+
+### Extra features
+* AMD/CommonJS support
+* Ability to customize cluster actions depending on the accuracy of the child pins.
+* Exposes more events: `onClusterClick`, `onClusterTransitionFinished`
+
+
+### Usage
+Create a new MarkerClusterGroup, add your markers to it, then add it to the map
+
+```javascript
+var markers = new L.MarkerClusterGroup();
+markers.addLayer(new L.Marker(getRandomLatLng(map)));
+... Add more layers ...
+map.addLayer(markers);
+```
---
+++ src/loader.js
@@ -0,0 +1,20 @@
+(function (factory, window) {
+    // define an AMD module that relies on 'leaflet'
+    if (typeof define === 'function' && define.amd) {
+        define(['leaflet'], factory);
+
+    // define a Common JS module that relies on 'leaflet'
+    } else if (typeof exports === 'object') {
+        if (typeof window !== 'undefined' && window.L) {
+            module.exports = factory(L);
+        } else {
+            module.exports = factory(require('leaflet'));
+        }
+    }
+
+    // attach your plugin to the global 'L' variable
+    if(typeof window !== 'undefined' && window.L){
+        window.L.Locate = factory(L);
+    }
+
+} (function (L) {
